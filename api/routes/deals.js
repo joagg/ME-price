742d46bffa233e2ec18300f5d14aeef943566e69
Johan Malmanger <johanmalmanger@Johans-MacBook-Pro-2.local>
2018-04-26 03:15:11 +0200

    Added feedback based on deal query

--- api/routes/deals.js
+++ api/routes/deals.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const emoji = require('node-emoji');
 
 router.get('/', (req, res, next) => {
   res.status(200).json({
@@ -24,6 +25,14 @@ router.post('/:dealId', (req, res, next) => {
     message: 'Deal details.. løø',
     dealId: req.params.dealId
   });
+  console.log('====================================================');
+  console.log('Prisjustering mottatt for vare: ' + req.params.dealId);
+  console.log('====================================================');
+  console.log('Tilbudet starter: ' +req.body.from_date);
+  console.log('Tilbudet varer til: '+req.body.to_date);
+  console.log('Pris i tilbudsperioden: ' + req.body.price + " kr");
+  console.log('Nedprising utført ' + emoji.emojify(':money_with_wings:')+ emoji.emojify(':money_with_wings:')+ emoji.emojify(':money_with_wings:'));
+  console.log('====================================================');
 });
 
 module.exports = router;
