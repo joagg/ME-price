ab94d4754cfd2caa5967c0c5e6c235c29ec2db54
Johan Malmanger <johanmalmanger@Johans-MacBook-Pro-2.local>
2018-04-26 03:26:06 +0200

    Added startup message

--- server.js
+++ server.js
@@ -3,4 +3,6 @@ const app = require('./app');
 const port = 3000;
 const server = http.createServer(app);
 
-server.listen(port);
+server.listen(port, () => {
+  console.log('Lytter etter prisendringer..');
+});
